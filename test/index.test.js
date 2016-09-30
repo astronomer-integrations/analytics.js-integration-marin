79a30a45e5ee7a1512db8c0e81e4ffd86a588e38
Alois Barreras <alois@vicino.io>
2016-09-30 00:08:46 -0400

    updated deps

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var Analytics = require('@segment/analytics.js-core').constructor;
-var integration = require('@segment/analytics.js-integration');
+var Analytics = require('@astronomerio/analytics.js-core').constructor;
+var integration = require('@astronomerio/analytics.js-integration');
 var sandbox = require('@segment/clear-env');
 var tester = require('@segment/analytics.js-integration-tester');
 var Marin = require('../lib/');
